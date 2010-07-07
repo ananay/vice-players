00153b758b40a43bf5fdda6802389b1328ac7ea9
ru.mysqld <ru.mysqld@4f1d208f-eed1-4694-e7b2-387ca239d83c>
2010-07-07 04:04:43 +0000

    Fixed linux compile

--- trunk/server/main.cpp
+++ trunk/server/main.cpp
@@ -229,3 +229,15 @@ void FilterInvalidNickChars(PCHAR szString)
 
 //----------------------------------------------------
 
+#ifndef _WIN32
+long GetTickCount()
+{
+        struct timeval tv;
+        if(gettimeofday(&tv, NULL) != 0)
+                return 0;
+
+        return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
+#endif
+
+//----------------------------------------------------
