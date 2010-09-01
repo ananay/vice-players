c34df15e2099081c9b898544e3180cbba585593d
ru.mysqld <ru.mysqld@4f1d208f-eed1-4694-e7b2-387ca239d83c>
2010-09-01 07:21:29 +0000

    modified rcon, added linux version

--- rcon/main.h
+++ rcon/main.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#ifdef WIN32
 #include "../raknet/WindowsIncludes.h"
 #include <conio.h>
-#include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdarg.h>
 #include <tchar.h>
 #include <process.h>
+#else
+#include "../server/system.h"
+#include "../raknet/Kbhit.h"
+#define Sleep(x) usleep(x * 1000)
+#endif
 
 #include "../raknet/RakPeer.h"
 #include "../raknet/BitStream.h"
