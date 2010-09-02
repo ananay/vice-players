defe65cc11b4f434671eefd30ee810bb10c66c08
jenksta <jenksta@4f1d208f-eed1-4694-e7b2-387ca239d83c>
2010-09-02 09:27:00 +0000

    Removed big dirty GPL license headers.

--- trunk/Client/game/scripting.h
+++ trunk/Client/game/scripting.h
@@ -1,7 +1,12 @@
-// PUBLIC DOMAIN
+//----------------------------------------------------------
 //
-//	Taken from spookie's speedo mod.
+// VC:MP Multiplayer Modification For GTA:VC
+// Copyright 2004-2005 SA:MP team
 //
+// File Author(s): spookie
+// License: Public Domain (Taken from spookie's speedo mod)
+//
+//----------------------------------------------------------
 
 #pragma once
 
