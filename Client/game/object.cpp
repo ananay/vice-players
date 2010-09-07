673c94db4bc7a7e61e1dd728ec63dd9544d62c98
jenksta <jenksta@4f1d208f-eed1-4694-e7b2-387ca239d83c>
2010-09-07 18:01:26 +0000

    Fixed the object code.

--- Client/game/object.cpp
+++ Client/game/object.cpp
@@ -24,33 +24,41 @@ extern CChatWindow   *pChatWindow;
 
 CObject::CObject(int iModel, Vector3 * vecPos, Vector3 * vecRot)
 {
-	DWORD dwReturnID     = 0;
-	m_dwGTAId            = 0;
+	// Reset the object index
+	m_dwGTAId = 0;
 
-	ScriptCommand(&create_object, iModel, vecPos->X, vecPos->Y, vecPos->Z, &dwReturnID);
+	// Create the object
+	ScriptCommand(&create_object, iModel, vecPos->X, vecPos->Y, vecPos->Z, &m_dwGTAId);
+	
+	// Did the object creation succeed?
+	if(m_dwGTAId)
+	{
+		// Set the entity pointer
+		SetEntity((ENTITY_TYPE *)CPools::GetObjectFromIndex(m_dwGTAId));
 
-	SetEntity((ENTITY_TYPE *)CPools::GetObjectFromIndex(dwReturnID));
-
-	//ScriptCommand(&set_object_rotation, dwReturnID, vecRot->X, vecRot->Y, vecRot->Z);
-
-	m_dwGTAId = dwReturnID;
+		// Set the object rotation
+		ScriptCommand(&set_object_rotation, m_dwGTAId, vecRot->X, vecRot->Y, vecRot->Z);
+	}
 }
 
 CObject::~CObject()
 {
-	ScriptCommand(&destroy_object, m_dwGTAId);
+	// Do we have a valid object index?
+	if(m_dwGTAId != 0)
+	{
+		// Destroy the object
+		ScriptCommand(&destroy_object, m_dwGTAId);
+	}
 }
 
-void CObject::SetObjectTargetable( )
+void CObject::SetObjectTargetable()
 {
 	ScriptCommand(&set_target_state, m_dwGTAId);
 }
 
-int CObject::HasObjectBeenDamaged( )
+int CObject::HasObjectBeenDamaged()
 {
 	int iDamage = 0;
-
 	ScriptCommand(&get_object_state, m_dwGTAId, &iDamage);
-
 	return iDamage;
 }
