1786de4c88bab0975c6adbe5b8c472a5a7b96410
ru.mysqld@gmail.com <ru.mysqld@gmail.com@4f1d208f-eed1-4694-e7b2-387ca239d83c>
2010-09-19 07:33:20 +0000

    a bit of cleanup

--- trunk/Server/StdInc.h
+++ trunk/Server/StdInc.h
@@ -1,3 +1,4 @@
+// Standard
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,35 +6,36 @@
 #include <string>
 #include <time.h>
 
-#include "../RakNet/RakPeer.h"
-#include "../RakNet/RPC4Plugin.h"
-#include "../RakNet/BitStream.h"
-
-#include "main.h"
-#include "CMasterList.h"
-
-#include "system.h"
-#include "CConfig.h"
-
-#ifdef WIN32
-#include <stdio.h>
-#include "../RakNet/WindowsIncludes.h"
-#else
+#ifndef WIN32
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <unistd.h>
 #define Sleep(x) usleep(x * 1000)
 #endif
 
+// RakNet
+#include "../RakNet/RakPeer.h"
+#include "../RakNet/RPC4Plugin.h"
+#include "../RakNet/BitStream.h"
+#include "../RakNet/RakPeerInterface.h"
+#include "../RakNet/WindowsIncludes.h"
+
+// TinyXML
+#include "../Shared/TinyXML/tinyxml.h"
+#include "../Shared/TinyXML/ticpp.h"
+
+// VCP
 #include "main.h"
+#include "system.h"
+#include "CConfig.h"
 #include "CNetGame.h"
 #include "CRcon.h"
 #include "CScripts.h"
 #include "CPlugins.h"
+#include "CMasterList.h"
+#include "sq_utils.h"
 
-#include "../RakNet/BitStream.h"
-#include "../RakNet/RakPeerInterface.h"
-
+// Squirrel
 #include "../squirrel/sqstdaux.h"
 #include "../squirrel/sqstdblob.h"
 #include "../squirrel/sqstdio.h"
@@ -42,8 +44,5 @@
 #include "../squirrel/sqstdsystem.h"
 #include "../squirrel/sqstring.h"
 
-#include "sq_utils.h"
-
-#include "../Shared/TinyXML/tinyxml.h"
-#include "../Shared/TinyXML/ticpp.h"
+// Shared
 #include "../Shared/CXML.h"
